49acd1af330f4d4e6bd2e178277d0a53164294a4
Bruno Michetti <brunomichettileites@gmail.com>
2019-01-30 14:11:00 -0300

    config bd y .gitignore

--- target/settings.py
+++ target/settings.py
@@ -73,8 +73,17 @@ WSGI_APPLICATION = 'target.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# use PostgreSQL database
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'target-db',
+        'USER': 'admin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'POST': '5432'
+    },
+    'original': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }

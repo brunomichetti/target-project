d16412ebb72305c3bd80dc59dcef8bd816a960db
Bruno Michetti <brunomichettileites@gmail.com>
2019-08-22 14:58:17 -0300

    move circleci var env to circleci config

--- target/settings/base.py
+++ target/settings/base.py
@@ -164,26 +164,31 @@ AUTH_PASSWORD_VALIDATORS = [
 # fb backend  app id
 FB_APP_ID = os.getenv('FB_APP_ID')
 SOCIALACCOUNT_PROVIDERS = \
-    {'facebook':
-     {'METHOD': 'oauth2',
-      'SCOPE': ['email', 'public_profile', 'user_friends'],
-      'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
-      'FIELDS': [
-          'id',
-          'email',
-          'name',
-          'first_name',
-          'last_name',
-          'verified',
-          'locale',
-          'timezone',
-          'link',
-          'gender',
-          'updated_time'],
-      'EXCHANGE_TOKEN': True,
-      'LOCALE_FUNC': lambda request: 'kr_KR',
-      'VERIFIED_EMAIL': False,
-      'VERSION': 'v2.4'}}
+    {
+        'facebook':
+            {
+                'METHOD': 'oauth2',
+                'SCOPE': ['email', 'public_profile', 'user_friends'],
+                'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+                'FIELDS': [
+                    'id',
+                    'email',
+                    'name',
+                    'first_name',
+                    'last_name',
+                    'verified',
+                    'locale',
+                    'timezone',
+                    'link',
+                    'gender',
+                    'updated_time'
+                ],
+                'EXCHANGE_TOKEN': True,
+                'LOCALE_FUNC': lambda request: 'kr_KR',
+                'VERIFIED_EMAIL': False,
+                'VERSION': 'v2.4'
+            }
+    }
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'users.serializers.CustomUserProfileSerializer'
